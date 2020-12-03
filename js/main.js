aa5840e618d2b21046df60255539914a66c40f83
Stibunoff <d.stibunoff@bk.ru>
2020-12-03 18:37:14 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -16,8 +16,15 @@ var reviewsSlider = new Swiper('.reviews-slider', {
 
   // Navigation arrows
   navigation: {
-    nextEl: '.hotel-slider__button-next',
-    prevEl: '.hotel-slider__button-prev',
+    nextEl: '.reviews-slider__button-next',
+    prevEl: '.reviews-slider__button-prev',
   },
   effect: "cube"
-  })
+})
+  
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function () {
+  console.log("Клик по кнопке меню");
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+})
