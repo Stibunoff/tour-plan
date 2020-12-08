00bf4e12c786efcf124c4477257ac64121aabc4b
Stibunoff <d.stibunoff@bk.ru>
2020-12-08 15:15:01 +0300

    create: add modal

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -23,8 +24,27 @@ var reviewsSlider = new Swiper('.reviews-slider', {
 })
   
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+var menuButton = $(".menu-button");
+menuButton.on('click', function () {
+  $(".navbar-bottom").toggleClass("navbar-bottom--visible");
 })
+  
+  var modalButton = $('[data-toggle="modal"]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass('modal__overlay--visible');
+    $(targetModal).find(".modal__dialog").addClass('modal__dialog--visible');
+  }
+  function closeModal(event) {
+    var targetModal = ($(this).attr("data-href"));
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+});
